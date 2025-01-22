6336ce1fad09f1507f52c31fa5ef879f3318f637
Hyacinthum-Polypus <18484965+Hyacinthum-Polypus@users.noreply.github.com>
2025-01-22 14:27:16 +1100

    Using curly braces and multiple components to make an animal list

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,28 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function ListItem(props) {
+  return <li>{props.animal}</li>
+}
+
+function List(props) {
+  return (
+    <ul>
+      {props.animals.map((animal) => {
+        return <ListItem key={animal} animal={animal} />;
+      })}
+    </ul>
+  )
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const animals = ["Lion", "Cow", "Snake", "Lizard"];
 
   return (
-    <>
-      <h1>Hello, World!</h1>
-    </>
+    <div>
+      <h1>Animals: </h1>
+      <List animals={animals}></List>
+    </div>
   )
 }
 
